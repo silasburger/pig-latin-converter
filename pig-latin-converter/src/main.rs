646fc5a1aebd057e7e2274dad20e96458f1c7378
Silas Burger <ybsilas@gmail.com>
2019-04-26 11:41:27 -0700

    trying to solve borrowing issues

--- pig-latin-converter/src/main.rs
+++ pig-latin-converter/src/main.rs
@@ -6,24 +6,25 @@ use std::collections::HashSet;
 fn convert_string(input: String) -> String {
     let words: Vec<&str> = input.split(' ').collect();
 
-    let result_vec: Vec<&str>;
+    let mut result_vec: Vec<&str> = Vec::new();
 
     let vowel_set: HashSet<&'static str> = [ "a", "e", "i", "o", "u", "A", "E", "I", "O", "U", "1", "2", "3", "4", "5", "6", "7", "8", "9" ].iter().cloned().collect();
 
+    let mut result_word = String::new();
+
     for word in words {
         let (letter, rest_of_word) = word.split_at(1);
-        let result_word = String::new();
 
         if vowel_set.contains(letter) {
             result_word.push_str(word);
             result_word.push_str("hay");
-            result_vec.push(&result_word);
         } else {
             result_word.push_str(rest_of_word);
             result_word.push_str(letter);
             result_word.push_str("ay");
-            result_vec.push(&result_word);
         };
+        
+        result_vec.push(&result_word);
 
     }
     return result_vec.join(" ");
@@ -48,14 +49,5 @@ fn main() {
 
         let result_str = convert_string(trimmed.to_string());
         println!("{}", result_str);
-
-        
-
-        // get input 
-        // if input is q then exit loop
-        // otherwise loop through each word and convert
-        // push each word to new string
-        // return that string
-        //start over
     }
 }
---
+++ pig-latin-converter/target/debug/deps/pig_latin_converter-3f1902f38a9a6e61.d
@@ -0,0 +1,5 @@
+/Users/lindadaignault/Documents/coding/rust/rust-tutorials/projects/pig-latin-converter/pig-latin-converter/target/debug/deps/pig_latin_converter-3f1902f38a9a6e61: src/main.rs
+
+/Users/lindadaignault/Documents/coding/rust/rust-tutorials/projects/pig-latin-converter/pig-latin-converter/target/debug/deps/pig_latin_converter-3f1902f38a9a6e61.d: src/main.rs
+
+src/main.rs:
